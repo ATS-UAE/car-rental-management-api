b6dee1b374dfe5bea3aae135db5b5ad293714e3b
Ramil Amparo <ramilamparo@gmail.com>
2020-05-11 07:11:59 +0400

    Build with latest typings.

--- dist/User.d.ts
+++ dist/User.d.ts
@@ -10,10 +10,10 @@ export declare class User {
     static getAll: (login: Authenticated) => Promise<User[]>;
     static create: (login: Authenticated, userData: UserServerParamsPost) => Promise<User>;
     update: (updatedVehicleData: import("./shared/typings").DatePropsToUnix<Pick<Pick<import("./shared/typings").UserAttributes, "password" | "blocked" | "clientId" | "username" | "firstName" | "lastName" | "email" | "mobileNumber" | "contractNo" | "objectNo" | "lastLogin" | "userImageSrc" | "licenseImageSrc" | "emailConfirmed" | "role" | "userCreatorId" | "timeZone">, never> & Pick<Partial<Pick<import("./shared/typings").UserAttributes, "password" | "blocked" | "clientId" | "username" | "firstName" | "lastName" | "email" | "mobileNumber" | "contractNo" | "objectNo" | "lastLogin" | "userImageSrc" | "licenseImageSrc" | "emailConfirmed" | "role" | "userCreatorId" | "timeZone">>, "password" | "blocked" | "clientId" | "username" | "firstName" | "lastName" | "email" | "mobileNumber" | "userImageSrc" | "licenseImageSrc" | "role" | "timeZone"> & {
-        categories: number[];
+        categories?: number[] | undefined;
     }>) => Promise<void>;
     static update: (login: Authenticated, userId: number, updatedVehicleData: import("./shared/typings").DatePropsToUnix<Pick<Pick<import("./shared/typings").UserAttributes, "password" | "blocked" | "clientId" | "username" | "firstName" | "lastName" | "email" | "mobileNumber" | "contractNo" | "objectNo" | "lastLogin" | "userImageSrc" | "licenseImageSrc" | "emailConfirmed" | "role" | "userCreatorId" | "timeZone">, never> & Pick<Partial<Pick<import("./shared/typings").UserAttributes, "password" | "blocked" | "clientId" | "username" | "firstName" | "lastName" | "email" | "mobileNumber" | "contractNo" | "objectNo" | "lastLogin" | "userImageSrc" | "licenseImageSrc" | "emailConfirmed" | "role" | "userCreatorId" | "timeZone">>, "password" | "blocked" | "clientId" | "username" | "firstName" | "lastName" | "email" | "mobileNumber" | "userImageSrc" | "licenseImageSrc" | "role" | "timeZone"> & {
-        categories: number[];
+        categories?: number[] | undefined;
     }>) => Promise<User>;
     destroy: () => Promise<void>;
     getCategories: () => Promise<Category[]>;
--- dist/shared/typings/api/User.d.ts
+++ dist/shared/typings/api/User.d.ts
@@ -2,10 +2,10 @@ import { UserAttributes, DatePropsToUnix, ServerResponse, RemoveImmutableSequeli
 export declare type UserServerResponseGet = ServerResponse<DatePropsToUnix<UserAttributes>>;
 export declare type UserServerResponseGetAll = ServerResponse<DatePropsToUnix<UserAttributes>[]>;
 export declare type UserServerParamsPatch = DatePropsToUnix<UseParameters<RemoveImmutableSequelizeProperties<UserAttributes>, never, "userImageSrc" | "timeZone" | "clientId" | "email" | "firstName" | "lastName" | "blocked" | "mobileNumber" | "licenseImageSrc" | "password" | "role" | "username"> & {
-    categories: number[];
+    categories?: number[];
 }>;
 export declare type UserServerParamsPost = DatePropsToUnix<UseParameters<RemoveImmutableSequelizeProperties<UserAttributes>, "clientId" | "email" | "username" | "firstName" | "lastName" | "password" | "mobileNumber" | "role", "userImageSrc" | "timeZone">> & {
-    categories: number[];
+    categories?: number[];
 };
 export declare type UserServerResponsePost = UserServerResponseGet;
 export declare type UserServerResponsePatch = UserServerResponseGet;
