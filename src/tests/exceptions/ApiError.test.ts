e384efcf956dc43e3dcb71eec2529f267cbbc94a
ramilamparo <ramilamparo@gmail.com>
2020-12-30 12:48:22 +0400

    Clean up.

--- src/tests/exceptions/ApiError.test.ts
+++
@@ -1,8 +0,0 @@
-import { ApiError } from "../../exceptions/ApiError";
-
-describe("ApiError", () => {
-	it("Extends error", () => {
-		const error = new ApiError("test");
-		expect(error instanceof Error).toBe(true);
-	});
-});
